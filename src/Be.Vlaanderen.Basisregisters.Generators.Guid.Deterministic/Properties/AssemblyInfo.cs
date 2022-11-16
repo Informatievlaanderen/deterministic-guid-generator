a9f8ce872013bbb918966a8ae6f00a31cdcb0cf4
kiryl0277 <109075027+kiryl0277@users.noreply.github.com>
2022-11-16 15:51:05 +0100

    update version

--- src/Be.Vlaanderen.Basisregisters.Generators.Guid.Deterministic/Properties/AssemblyInfo.cs
+++ src/Be.Vlaanderen.Basisregisters.Generators.Guid.Deterministic/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyProduct("Vlaamse basisregisters")]
 [assembly: AssemblyDescription("Create a deterministic GUID based on namespace Guid, a string and an optional version.")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
 [assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
 [assembly: AssemblyCompany("Vlaamse overheid")]
 
